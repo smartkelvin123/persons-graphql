cd68eaef1659e13c01dfb4d3c35efcb3509993fa
unknown <smartkelvin66@gmail.com>
2023-07-05 15:00:14 +0100

    updated app

--- graphql/src/App.js
+++ graphql/src/App.js
@@ -1,30 +1,36 @@
-import { gql, useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
 import Persons from "./component/Persons";
 import PersonForm from "./component/PersonForm";
+import { ALL_PERSONS } from "./queries";
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
   }
-`;
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS, {
-    pollInterval: 2000,
-  });
+  const [errorMessage, setErrorMessage] = useState(null);
+  const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };
--- graphql/src/component/PersonForm.js
+++ graphql/src/component/PersonForm.js
@@ -3,7 +3,7 @@ import { useMutation } from "@apollo/client";
 
 import { CREATE_PERSON, ALL_PERSONS } from "../queries";
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [street, setStreet] = useState("");
@@ -11,6 +11,13 @@ const PersonForm = () => {
 
   const [createPerson] = useMutation(CREATE_PERSON, {
     refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => {
+      const errors = error.graphQLErrors[0].extensions.error.errors;
+      const messages = Object.values(errors)
+        .map((e) => e.message)
+        .join("\n");
+      setError(messages);
+    },
   });
 
   const submit = async (event) => {
